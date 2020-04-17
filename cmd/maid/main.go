b2043a121891ad84012f8f5b350840b717d8231e
Naoki Ikeguchi <root@siketyan.dev>
2020-04-17 15:32:46 +0900

    Add panic handling

--- cmd/maid/main.go
+++ cmd/maid/main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"github.com/brokenManager/maid/pkg/async"
 	"github.com/brokenManager/maid/pkg/discord"
+	"log"
 )
 
 func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
 	bot := discord.CreateBot("")
 	bot.Start()
 	async.Wait()
